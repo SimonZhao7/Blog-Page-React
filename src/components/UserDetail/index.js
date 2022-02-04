f60a249127abd687ba6e01edc0eabb2bac548466
Simon Zhao <68301003+SimonZhao7@users.noreply.github.com>
2022-02-03 21:49:31 -0800

    Reformat Files For New Feature

--- src/components/UserDetail/index.js
+++ src/components/UserDetail/index.js
@@ -1,80 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from 'react-router-dom';
 import { ThinButton } from '../../GlobalStyle';
 import { UserInfo, UserNumbers } from './UserDetail.styles';
 import { useAppContext } from "../../context";
 
 
-const UserDetail = ({ id, username }) => {
-    const { user, token } = useAppContext()
-    const [followers, setFollowers] = useState([])
-    const [following, setFollowing] = useState([])
-    const [followStatus, setFollowStatus] = useState('Follow')
-
-    const handleFollow = async () => {
-        const body = new FormData()
-        body.append('user', user.id)
-        body.append('following', id)
-        try {
-            fetch(`http://localhost:8000/api/UserFollowingAPI/`, {
-                method: 'POST',
-                headers: {
-                    Authorization: `Token ${token}`
-                },
-                body: body
-            })
-                .then(response => response.json())
-                .then(data => {
-                    setFollowers([...followers, data])
-                    setFollowStatus('Following')
-                })
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    const handleUnfollow = () => {
-        const followToBeDeleted = followers.filter(item => item.user === user.id)
-        followToBeDeleted.forEach(follow => {
-            try {
-                fetch(`http://localhost:8000/api/UserFollowingAPI/${follow.id}/`, {
-                    method: 'DELETE',
-                    headers: {Authorization: `Token ${token}`}
-                })
-            } catch (error) {
-                console.log(error)
-            }
-        })
-        setFollowers(followers.filter(item => item.user !== user.id))
-        setFollowStatus('Follow')
-    }
-
-    useEffect(() => {
-        const fetchData = () => {
-            fetch(`http://localhost:8000/api/UserFollowingAPI/following_id=${id}/`, {
-                headers: {Authorization: `Token ${token}`}
-            })
-                .then(response => response.json())
-                .then(data => {
-                    setFollowers([...data])
-                    setFollowStatus(data.filter(item => item.user === user.id).length > 0 ? 'Following': 'Follow')
-                })
-                .catch((error) => {
-                    console.error(error);
-                })
-    
-            fetch(`http://localhost:8000/api/UserFollowingAPI/user_id=${id}/`, {
-                headers: {Authorization: `Token ${token}`}
-            })
-                .then(response => response.json())
-                .then(data => setFollowing(data))
-                .catch((error) => {
-                    console.error(error);
-                })
-        }
-        fetchData()
-    }, [id, token, user.id])
-
+const UserDetail = ({ viewedUser, followers, following, followStatus, handleFollow, handleUnfollow }) => {
+    const { user } = useAppContext()
+    const { id, username } = viewedUser
     return (
         <UserInfo>
             <div>
@@ -98,7 +31,7 @@ const UserDetail = ({ id, username }) => {
                 {user.username === username
                     ? <ThinButton width='90%'><Link to='/settings'>Settings</Link></ThinButton>
                     : <ThinButton width='90%' 
-                        onClick={followStatus === 'Following' ? handleUnfollow : handleFollow}>{followStatus}</ThinButton>
+                        onClick={() => {followStatus === 'Following' ? handleUnfollow() : handleFollow(id)}}>{followStatus}</ThinButton>
                 }
             </div>
         </UserInfo>
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -10,41 +10,106 @@ const Profile = () => {
     const { user, token } = useAppContext()
     const [viewedUser, setViewedUser] = useState()
     const [loading, setLoading] = useState(true)
+    // The viewed user's data
+    const [followers, setFollowers] = useState([])
+    const [following, setFollowing] = useState([])
+    const [followStatus, setFollowStatus] = useState('Follow')
+    // Url Params
     const usernameParam = useParams('username').username
 
+    const handleFollow = async (id) => {
+        const body = new FormData()
+        body.append('user', user.id)
+        body.append('following', id)
+        try {
+            fetch(`http://localhost:8000/api/UserFollowingAPI/`, {
+                method: 'POST',
+                headers: {
+                    Authorization: `Token ${token}`
+                },
+                body: body
+            })
+                .then(response => response.json())
+                .then(data => {
+                    setFollowers([...followers, data])
+                    setFollowStatus('Following')
+                })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handleUnfollow = () => {
+        const followToBeDeleted = followers.filter(item => item.user === user.id)
+        followToBeDeleted.forEach(follow => {
+            try {
+                fetch(`http://localhost:8000/api/UserFollowingAPI/${follow.id}/`, {
+                    method: 'DELETE',
+                    headers: {Authorization: `Token ${token}`}
+                })
+            } catch (error) {
+                console.log(error)
+            }
+        })
+        setFollowers(followers.filter(item => item.user !== user.id))
+        setFollowStatus('Follow')
+    }
+
     useEffect(() => {
-        const fetchUserDetails = async () => {
+        const fetchData = async () => {
             const response = await fetch(`http://localhost:8000/api/CustomUserAPI/username=${usernameParam}/`, {
                 headers: {Authorization: `Token ${token}`}
             })
             const data = await response.json()
+
+            fetch(`http://localhost:8000/api/UserFollowingAPI/following_id=${data.id}/`, {
+                headers: {Authorization: `Token ${token}`}
+            })
+                .then(response => response.json())
+                .then(data => {
+                    setFollowers([...data])
+                    setFollowStatus(data.filter(item => item.user === user.id).length > 0 ? 'Following': 'Follow')
+                })
+                .catch((error) => {
+                    console.error(error);
+                })
+    
+            fetch(`http://localhost:8000/api/UserFollowingAPI/user_id=${data.id}/`, {
+                headers: {Authorization: `Token ${token}`}
+            })
+                .then(response => response.json())
+                .then(data => setFollowing(data))
+                .catch((error) => {
+                    console.error(error);
+                })
             if (isMounted) {
                 setViewedUser(data)
                 setLoading(false)
             }
+            return () => {isMounted = false}
         }
-        setLoading(true)
         let isMounted = true
-        fetchUserDetails()
-        return () => {isMounted = false}
-    }, [token, usernameParam])
-
-    if (!user) return (<Navigate to='/login' />)
-    
-    if (loading) {
-        return (
-            <ContentWrapper>
-                <Spinner></Spinner>
-            </ContentWrapper>
-        )
-    }
+        fetchData()
+    }, [token, user.id, usernameParam])
 
     return (
         <ContentWrapper>
+            {!user && <Navigate to='/login' />}
+            {loading 
+            ? <Spinner></Spinner>
+            : 
             <UserContent>
                 <img src={`http://localhost:8000${viewedUser.profile_picture}`} alt='profile-pic' />
-                <UserDetail {...viewedUser} />
+                <UserDetail 
+                    viewedUser={viewedUser}
+                    followers={followers} 
+                    following={following} 
+                    followStatus={followStatus} 
+                    handleFollow={handleFollow} 
+                    handleUnfollow={handleUnfollow}
+                />
             </UserContent>
+            }
         </ContentWrapper>
     )
 }
